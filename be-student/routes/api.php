86405f0946ed17c209fd3d90a1fea97d133326f0
Felipe <luizfelipesouza123@gmail.com>
2023-04-23 17:54:10 -0300

    BackEnd API routes (all)

--- be-student/routes/api.php
+++ be-student/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('students', 'App\Http\Controllers\AlunoController@index');
+
+Route::get('students/show', 'App\Http\Controllers\AlunoController@show');
+
+Route::post('/students/store', 'App\Http\Controllers\AlunoController@store');
+
+Route::put('/students/{RA}', 'App\Http\Controllers\AlunoController@update');
+
+Route::delete('/students/{RA}', 'App\Http\Controllers\AlunoController@destroy');
